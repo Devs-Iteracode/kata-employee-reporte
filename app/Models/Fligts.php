74f19bc79028807e361fa4dba7dac80e015598e3
Florent Bruziaux <florent@iteracode.fr>
2023-11-06 14:02:57 +0100

    work-in-progress

---
+++ app/Models/Fligts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fligts extends Model
+{
+    use HasFactory;
+}
